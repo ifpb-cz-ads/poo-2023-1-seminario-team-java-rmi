e53259ad2a50dbff120c85af18cd77bb812ab29f
IagoGOOl <iago.gool@gmail.com>
2023-07-03 18:14:21 -0300

    Atualizacao do cliente, craindo menu

--- Exemplo 2/Cliente.java
+++ Exemplo 2/Cliente.java
@@ -8,16 +8,41 @@ public class Cliente {
 
             Formulas formulas = (Formulas) Naming.lookup(nomeObjeto);
 
-            System.out.println("O primeiro numeros que voce deseja:");
-            double numero1 = sc.nextDouble();
 
-            System.out.println("Os segundo que voce deseja:");
-            double numero2 = sc.nextDouble();
+            System.out.println("Calcular a Area do Retangulo: digite 1");
+            System.out.println("Calcular a Area do Quadrado: digite 2");
+            System.out.println("Calcular a Area de uma Cicuferencia: digite 3");
+            System.out.println("Sair: digite 4");
+            int opcao = 0;
+            while ( opcao != 4){
+            System.out.println("O numero que voce deseja: ");
+             opcao = sc.nextInt();
+            switch (opcao) {
+                case 1 :
+                System.out.print("O primeiro numeros que voce deseja:");
+                double numero1 = sc.nextDouble();
+                System.out.print("O primeiro numeros que voce deseja:");
+                double numero2 = sc.nextDouble();
+                System.out.println("O Resultado da Area é Retangulo: " + formulas.Retangulo(numero1, numero2));
+                break;
+                case 2:
+                System.out.print("O primeiro numeros que voce deseja:");
+                double lado = sc.nextDouble();
+                System.out.println("O Resultado da Area é Quadrado: "  + formulas.Quadrado(lado));
+                break;
+                case 3:
+                System.out.print("O primeiro numeros que voce deseja:");
+                double raio = sc.nextDouble();
+                System.out.println("O Resultado da Area é Circulo: " + formulas.Circulo(raio));
+                break;
+                case 4: 
+                System.out.println("obrigador !!!");
+                break;
+                default:
+                System.out.println("invalido");
+            }
 
-            System.out.println("O Resultado da perimetro é Retangulo: " + formulas.Retangulo(numero1, numero2));
-            System.out.println("O Resultado da perimetro é Quadrado: " + numero1 + " " + formulas.Quadrado(numero1));
-            System.out.println("O Resultado da perimetro é Circulo: " + numero1 + " " + formulas.Circulo(numero1));
-
+            }
             sc.close();
         }
     
