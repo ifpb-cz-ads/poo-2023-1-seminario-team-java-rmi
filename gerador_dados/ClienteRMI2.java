d2c851ca46f93cd22e659bb898d21782eda86af5
adriellyEster <adriellyester9@gmail.com>
2023-07-03 18:54:23 -0300

    criando segundo cliente do servidor

---
+++ gerador_dados/ClienteRMI2.java
@@ -0,0 +1,56 @@
+import java.rmi.Naming;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+public class ClienteRMI2 {
+    public static void main(String[] args)  throws Exception{
+        String objNome = "rmi://localhost/dados";
+        IPessoa pessoa = (IPessoa) Naming.lookup(objNome);
+
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean menu = true;
+
+        while(menu){
+            
+            System.out.println("selecione uma opção");
+            System.out.println("1 - prever o futuro de alguem");
+            System.out.println("2 - encerrar o programa\n");
+            byte opcao = sc.nextByte();
+
+            switch(opcao){
+                case 1:
+                    
+                    Random num = new Random();
+                    System.out.println("-------------------------------------------");
+                    System.out.println("nome: "+pessoa.gerarNome());
+                    System.out.println("profissão "+pessoa.gerarProfissao());
+                    if (num.nextInt(10) <= 5 ){
+                        System.out.println("foi expulso(a), pelo motivo de: "+pessoa.gerarMotivoExpulsao());
+                    }
+                    else{
+                        System.out.println("não foi expulso(a)");
+                    }
+                    System.out.println("---------------------------------------------");
+
+                break;
+
+                case 2:
+                    
+                    System.out.println("obrigada por usar o programa");
+                    menu = false;
+                    break;
+
+                
+            }
+
+
+        }
+        
+       
+
+    }
+}
