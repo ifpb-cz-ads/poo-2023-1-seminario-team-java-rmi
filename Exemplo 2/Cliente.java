da1554ad285c01e1674dfb6581aaf87a61ac2720
IagoGOOl <iago.gool@gmail.com>
2023-07-03 17:07:11 -0300

    melhorando a interação com o usuario

--- Exemplo 2/Cliente.java
+++ Exemplo 2/Cliente.java
@@ -1,12 +1,24 @@
 import java.rmi.Naming;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
             String nomeObjeto = "rmi/localhost/geo";
 
             Formulas formulas = (Formulas) Naming.lookup(nomeObjeto);
 
-            System.out.println("O Resultado da soma é: " + formulas.Circulo(5));
+            System.out.println("O primeiro numeros que voce deseja:");
+            double numero1 = sc.nextDouble();
+
+            System.out.println("Os segundo que voce deseja:");
+            double numero2 = sc.nextDouble();
+
+            System.out.println("O Resultado da perimetro é Retangulo: " + formulas.Retangulo(numero1, numero2));
+            System.out.println("O Resultado da perimetro é Quadrado: " + numero1 + " " + formulas.Quadrado(numero1));
+            System.out.println("O Resultado da perimetro é Circulo: " + numero1 + " " + formulas.Circulo(numero1));
+
+            sc.close();
         }
     
 }
